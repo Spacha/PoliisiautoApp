f659d071d103ed5eb08bee3ca63299406d6ed20c
Essi Passoja <essi.passoja@gmail.com>
2022-10-24 16:30:20 +0300

    Make home page

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -3,7 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:url_launcher/link.dart';
+// import 'package:url_launcher/link.dart';
 
 import '../auth.dart';
 import '../routing.dart';
@@ -18,23 +18,23 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Align(
-              alignment: Alignment.topCenter,
-              child: ConstrainedBox(
-                constraints: const BoxConstraints(maxWidth: 400),
-                child: const Card(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 18, horizontal: 12),
-                    child: HomeContent(),
-                  ),
-                ),
+    body: SafeArea(
+      child: SingleChildScrollView(
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 400),
+            child: const Card(
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 18, horizontal: 12),
+                child: HomeContent(),
               ),
             ),
           ),
         ),
-      );
+      ),
+    ),
+  );
 }
 
 class HomeContent extends StatelessWidget {
@@ -44,52 +44,105 @@ class HomeContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Column(
-        children: [
-          ...[
-            Text(
-              'Settings',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                PoliisiautoAuthScope.of(context).signOut();
-              },
-              child: const Text('Sign out'),
-            ),
-            Link(
-              uri: Uri.parse('/report/0'),
-              builder: (context, followLink) => TextButton(
-                onPressed: followLink,
-                child: const Text('Go directly to /report/0 (Link)'),
+    children: [
+      ...[
+        Image.asset(
+            "graphics/logo-text-1x.png",
+            height: 200,
+            width: 200,
+          ),
+        Padding(
+            padding: const EdgeInsets.only(top: 50, bottom: 10),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(80),
+                backgroundColor: const Color.fromARGB(255, 158, 29, 20),
+                textStyle: const TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold
+                ),
               ),
+              child: const Text('SOS'),
             ),
-            TextButton(
-              child: const Text('Go directly to /report/0 (RouteState)'),
-              onPressed: () {
-                RouteStateScope.of(context).go('/report/0');
-              },
-            ),
-          ].map((w) => Padding(padding: const EdgeInsets.all(8), child: w)),
-          TextButton(
-            onPressed: () => showDialog<String>(
-              context: context,
-              builder: (context) => AlertDialog(
-                title: const Text('Alert!'),
-                content: const Text('The alert description goes here.'),
-                actions: [
-                  TextButton(
-                    onPressed: () => Navigator.pop(context, 'Cancel'),
-                    child: const Text('Cancel'),
-                  ),
-                  TextButton(
-                    onPressed: () => Navigator.pop(context, 'OK'),
-                    child: const Text('OK'),
-                  ),
-                ],
+          ),
+          Text(
+          'Tervetuloa Maija Malli!',
+          style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(0),
+            child: OutlinedButton(
+              onPressed: () async {RouteStateScope.of(context).go('/signin');},
+              style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                primary: Colors.white,
+                backgroundColor: const Color.fromARGB(255, 97, 144, 184),
+                textStyle: const TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
+              child: const Text('Kirjoita uusi raportti'),
             ),
-            child: const Text('Show Dialog'),
-          )
-        ],
-      );
+          ),
+          Padding(
+            padding: const EdgeInsets.all(0),
+            child: OutlinedButton(
+              onPressed: () {PoliisiautoAuthScope.of(context).signOut();},
+              style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                primary: Colors.white,
+                backgroundColor: const Color.fromARGB(255, 97, 144, 184),
+                textStyle: const TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              child: const Text('Kirjaudu ulos'),
+            ),
+          ),
+        /*Link(
+          uri: Uri.parse('/report/0'),
+          builder: (context, followLink) => TextButton(
+            onPressed: followLink,
+            child: const Text('Go directly to /report/0 (Link)'),
+          ),
+        ),
+        */
+        /*TextButton(
+          child: const Text('Go directly to /report/0 (RouteState)'),
+          onPressed: () {
+            RouteStateScope.of(context).go('/report/0');
+          },
+        ),
+        */
+      ].map((w) => Padding(padding: const EdgeInsets.all(8), child: w)),
+      /*TextButton(
+        onPressed: () => showDialog<String>(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Alert!'),
+            content: const Text('The alert description goes here.'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context, 'Cancel'),
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context, 'OK'),
+                child: const Text('OK'),
+              ),
+            ],
+          ),
+        ),
+        child: const Text('Show Dialog'),
+      )*/
+    ],
+  );
 }
