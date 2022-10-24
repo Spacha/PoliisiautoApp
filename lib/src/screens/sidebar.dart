cf40efdb392677d9dd87ccdcfffffee173ea720f
Essi Passoja <essi.passoja@gmail.com>
2022-10-24 17:57:25 +0300

    Decorate sidebar and add routing

--- lib/src/screens/sidebar.dart
+++ lib/src/screens/sidebar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../auth.dart';
+import '../routing.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
@@ -6,30 +8,84 @@ class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          const DrawerHeader(
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 32, 112, 232),
+      child: Container(
+        color: const Color.fromARGB(255, 112, 162, 237),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const SizedBox(
+              height: 90,
+              child: DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 32, 112, 232),
+                ),
+                child: Text('Maijan Poliisiauto', 
+                  style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 25
+                  ),
+                ),
+              ),
             ),
-            child: Text('Drawer Header'),
-          ),
-          ListTile(
-            title: const Text('Item 1'),
-            onTap: () {
-              // Update the state of the app.
-              // ...
-            },
-          ),
-          ListTile(
-            title: const Text('Item 2'),
-            onTap: () {
-              // Update the state of the app.
-              // ...
-            },
-          ),
-        ],
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Etusivu'),
+              textColor: Colors.white70,
+              onTap: () {
+                RouteStateScope.of(context).go('/home');
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.border_color),
+              title: const Text('Kirjoita uusi raportti'),
+              textColor: Colors.white70,
+              onTap: () {
+                RouteStateScope.of(context).go('/reports/popular');  // TODO: change route
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.notifications),
+              title: const Text('Minun ilmoitukset'),
+              textColor: Colors.white70,
+              onTap: () {
+                RouteStateScope.of(context).go('/reports/popular');  // TODO: change route
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.message),
+              title: const Text('Viestit'),
+              textColor: Colors.white70,
+              onTap: () {
+                RouteStateScope.of(context).go('/reports/popular');  // TODO: change route
+              },
+            ),
+            const SizedBox(height: 20),
+            const Divider(color: Colors.white70),
+            const SizedBox(height: 20),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text('Asetukset'),
+              textColor: Colors.white70,
+              onTap: () {
+                RouteStateScope.of(context).go('/reports/popular');  // TODO: change route
+              },
+            ),ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Minun tiedot'),
+              textColor: Colors.white70,
+              onTap: () {
+                RouteStateScope.of(context).go('/authors');  // TODO: change route
+              },
+            ),ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('Kirjaudu ulos'),
+              textColor: Colors.white70,
+              onTap: () {
+                PoliisiautoAuthScope.of(context).signOut();
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
