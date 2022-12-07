c5ae84159554817d64f64a9cb00bc07793eee185
Spacha <miikasikala96@gmail.com>
2022-12-07 07:09:46 +0200

    Various visual improvements to the drawer
    * Removed unnecessary container
    * Fixed tile highlight color
    * Fixed tile color in general; tile interaction effects were not visible
    * Reordered tiles for more logical order

--- lib/src/widgets/drawer.dart
+++ lib/src/widgets/drawer.dart
@@ -7,7 +7,7 @@ import '../auth.dart';
 class PoliisiautoDrawer extends StatelessWidget {
   const PoliisiautoDrawer({super.key});
 
-  final Color tileHighlightColor = const Color.fromARGB(255, 230, 230, 230);
+  final Color tileHighlightColor = const Color.fromARGB(255, 88, 145, 230);
 
   @override
   Widget build(BuildContext context) {
@@ -18,104 +18,98 @@ class PoliisiautoDrawer extends StatelessWidget {
       // Add a ListView to the drawer. This ensures the user can scroll
       // through the options in the drawer if there isn't enough vertical
       // space to fit everything.
-      child: Container(
-        color: const Color.fromARGB(255, 112, 162, 237),
-        child: ListView(
+      backgroundColor: const Color.fromARGB(255, 112, 162, 237),
+      child: ListView(
         // Important: Remove any padding from the ListView.
-        
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: const BoxDecoration(
-                color: Color.fromARGB(255, 32, 112, 232),
-              ),
-              child: Text('Hei ${getAuth(context).user!.name}!',
-                  style: const TextStyle(color: Colors.white),
-                  textScaleFactor: 1.5,
-                  textAlign: TextAlign.center,
-                  ),
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 32, 112, 232),
             ),
-            ListTile(
-              leading: const Icon(Icons.house_outlined),
-              title: const Text('Etusivu'),
-              iconColor: Colors.white,
-              textColor: Colors.white,
-              tileColor:
-                  (selectedIndex == 0) ? tileHighlightColor : Colors.transparent,
-              onTap: () {
-                // Update the state of the app
-                routeState.go('/home');
-                // Then close the drawer
-                Navigator.pop(context);
-              },
+            child: Text(
+              'Hei ${getAuth(context).user!.name}!',
+              style: const TextStyle(color: Colors.white),
+              textScaleFactor: 1.5,
+              textAlign: TextAlign.center,
             ),
-            ListTile(
-                leading: const Icon(Icons.border_color),
-                title: const Text('Tee uusi ilmoitus'),
-                iconColor: Colors.white,
-                textColor: Colors.white,
-                onTap: () {
-                  // Update the state of the app
-                  routeState.go('/new_report');
-                  // Then close the drawer
-                  Navigator.pop(context);
-                },
-              ),
-            ListTile(
-              leading: const Icon(Icons.speaker_notes_outlined),
-              title: const Text('Minun ilmoitukset'),
-              iconColor: Colors.white,
-              textColor: Colors.white,
-              tileColor:
-                  (selectedIndex == 1) ? tileHighlightColor : Colors.transparent,
-              onTap: () {
-                // Update the state of the app
-                routeState.go('/reports');
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.settings),
-              title: const Text('Asetukset'),
-              iconColor: Colors.white,
-              textColor: Colors.white,
-              onTap: () {
-                // Update the state of the app
-                routeState.go('/settings');
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.info_outlined),
-              title: const Text('Tietoa sovelluksesta'),
-              iconColor: Colors.white,
-              textColor: Colors.white,
-              tileColor:
-                  (selectedIndex == 2) ? tileHighlightColor : Colors.transparent,
-              onTap: () {
-                // Update the state of the app
-                routeState.go('/information');
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            const Divider(color: Color.fromARGB(255, 193, 193, 193)),
-            ListTile(
-              leading: const Icon(Icons.logout_outlined),
-              title: const Text('Kirjaudu ulos'),
-              iconColor: Colors.white,
-              textColor: Colors.white,
-              onTap: () {
-                // Update the state of the app
-                getAuth(context).signOut();
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.house_outlined),
+            title: const Text('Etusivu'),
+            iconColor: Colors.white,
+            textColor: Colors.white,
+            tileColor:
+                (selectedIndex == 0) ? tileHighlightColor : Colors.transparent,
+            onTap: () {
+              // Update the state of the app
+              routeState.go('/home');
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.speaker_notes_outlined),
+            title: isTeacher(context)
+                ? const Text('Minun ilmoitukset')
+                : const Text('Ilmoitukset'),
+            iconColor: Colors.white,
+            textColor: Colors.white,
+            tileColor:
+                (selectedIndex == 1) ? tileHighlightColor : Colors.transparent,
+            onTap: () {
+              // Update the state of the app
+              routeState.go('/reports');
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+
+          // TODO: Add help screen (selectedIndex = 2)
+
+          const Divider(color: Color.fromARGB(255, 193, 193, 193)),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text('Asetukset'),
+            iconColor: Colors.white,
+            textColor: Colors.white,
+            tileColor:
+                (selectedIndex == 3) ? tileHighlightColor : Colors.transparent,
+            onTap: () {
+              // Update the state of the app
+              routeState.go('/settings');
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.info_outlined),
+            title: const Text('Tietoa sovelluksesta'),
+            iconColor: Colors.white,
+            textColor: Colors.white,
+            tileColor:
+                (selectedIndex == 4) ? tileHighlightColor : Colors.transparent,
+            onTap: () {
+              // Update the state of the app
+              routeState.go('/information');
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+          const Divider(color: Color.fromARGB(255, 193, 193, 193)),
+          ListTile(
+            leading: const Icon(Icons.logout_outlined),
+            title: const Text('Kirjaudu ulos'),
+            iconColor: Colors.white,
+            textColor: Colors.white,
+            onTap: () {
+              // Update the state of the app
+              getAuth(context).signOut();
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+        ],
       ),
     );
   }
@@ -123,7 +117,9 @@ class PoliisiautoDrawer extends StatelessWidget {
   int _getSelectedIndex(String pathTemplate) {
     if (pathTemplate.startsWith('/home')) return 0;
     if (pathTemplate.startsWith('/reports')) return 1;
-    if (pathTemplate.startsWith('/information')) return 2;
+    if (pathTemplate.startsWith('/help')) return 2;
+    if (pathTemplate.startsWith('/settings')) return 3;
+    if (pathTemplate.startsWith('/information')) return 4;
 
     return -1;
   }
