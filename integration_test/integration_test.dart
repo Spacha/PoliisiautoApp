97ecaa1539e5cb6ff5eae086e4ec9b366bea6ff4
Essi Passoja <essi.passoja@gmail.com>
2022-12-11 14:51:17 +0200

    Add test for emergency floating button

--- integration_test/integration_test.dart
+++ integration_test/integration_test.dart
@@ -9,6 +9,10 @@ void main() {
   final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
 
+  // tester.pumpAndSettle has Duration (1s) set for tester friendliness, it is
+  // then easier to follow. Duration is only necessary when the app is launched
+  // and we need to wait for the splash screen to close (set to 5s). Another option
+  // is to make a splash screen wait.
   group('end-to-end test', () {
     testWidgets('Forgot password ', (tester) async {app.main();
       await tester.pumpAndSettle(const Duration(seconds: 5));
@@ -17,13 +21,13 @@ void main() {
       expect(find.text('Unohtuiko salasana?'), findsOneWidget);
       final Finder forgotPasswordButton = find.text('Unohtuiko salasana?');
       await tester.tap(forgotPasswordButton);
-      await tester.pumpAndSettle(const Duration(seconds: 2));
+      await tester.pumpAndSettle(const Duration(seconds: 1));
 
       // Check that the screen is correct and return to login page
       expect(find.text('Unohditko salasanasi?'), findsOneWidget);
       final Finder goBack = find.byIcon(Icons.arrow_back);
       await tester.tap(goBack);
-      await tester.pumpAndSettle(const Duration(seconds: 2));
+      await tester.pumpAndSettle(const Duration(seconds: 1));
 
       // Confirm we are back to the front page.
       expect(find.text('Kirjaudu'), findsOneWidget);
@@ -33,19 +37,41 @@ void main() {
       expect(find.text('Kirjaudu sisään'), findsOneWidget);
 
       // Try logging in with test credentials.
-
       final Finder debugTeacherButton = find.byKey(const ValueKey("debug teacher"));
       await tester.tap(debugTeacherButton);
-      await tester.pumpAndSettle(const Duration(seconds: 2));
+      await tester.pumpAndSettle(const Duration(seconds: 1));
       /* USE THESE LINES FOR LOG IN ONCE DEBUG USERS ARE REMOVED FROM THE LOG IN -PAGE:
       await tester.enterText(find.byKey(const ValueKey("e-mail")), 'olli.o@esimerkki.fi');
       await tester.enterText(find.byKey(const ValueKey("password")), 'salasana');
       */
       final Finder loginButton2 = find.text('Kirjaudu');
       await tester.tap(loginButton2);
-      await tester.pumpAndSettle(const Duration(seconds: 2));
-      expect(find.text('Etusivu'), findsOneWidget);
+      await tester.pumpAndSettle(const Duration(seconds: 1));
 
+      // Test emergency floating button on front page
+      for (var i=0; i<2; i++) {
+        expect(find.text('Etusivu'), findsOneWidget);
+        expect(find.byType(FloatingActionButton), findsOneWidget);
+        await tester.tap(find.byType(FloatingActionButton));
+        await tester.pumpAndSettle(const Duration(seconds: 1));
+        expect(find.byType(AlertDialog), findsOneWidget);
+        expect(find.text('Oletko varma että haluat lähettää hätäilmoituksen?'), findsOneWidget);
+        if (i==0) {
+          final Finder cancelEmergencyAction = find.text('Peruuta');
+          await tester.tap(cancelEmergencyAction);
+          await tester.pumpAndSettle(const Duration(seconds: 1));
+        }
+        else {
+          final Finder makeEmergencyAction = find.text('Olen varma');
+          await tester.tap(makeEmergencyAction);
+          await tester.pumpAndSettle(const Duration(seconds: 1));
+        }}
+      expect(find.text('Lähetetään hätäilmoitusta'), findsOneWidget);
+      // TODO: make tests for Voice message and Video when they are available.
+      final Finder endBroadcast = find.text('Lopeta lähetys');
+      await tester.tap(endBroadcast);
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+      
       /*// Test New report button on front page, fill in information and send the report.
       final Finder newReportButton = find.text('Tee uusi ilmoitus');
       await tester.tap(newReportButton);
