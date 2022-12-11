b64e608faed3f540ee15988c2cb44ba996382cd8
Essi Passoja <essi.passoja@gmail.com>
2022-12-11 21:21:38 +0200

    Make test for new report - v1

--- integration_test/integration_test.dart
+++ integration_test/integration_test.dart
@@ -3,7 +3,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'package:poliisiauto/main.dart' as app;
-// import 'package:poliisiauto/src/app.dart';
 
 void main() {
   final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
@@ -33,7 +32,7 @@ void main() {
       expect(find.text('Kirjaudu'), findsOneWidget);
   });
   
-    testWidgets('Home page with teacher -user.', (tester) async {app.main();
+    testWidgets('Home page with test -user.', (tester) async {app.main();
       await tester.pumpAndSettle(const Duration(seconds: 5));
       expect(find.text('Kirjaudu sisään'), findsOneWidget);
 
@@ -78,13 +77,14 @@ void main() {
       expect(find.text('Kirjaudu sisään'), findsOneWidget);
     });
 
-    testWidgets('Sidebar with teacher -user.', (tester) async {app.main();
+    testWidgets('Sidebar with test -user.', (tester) async {app.main();
       await tester.pumpAndSettle(const Duration(seconds: 5));
       expect(find.text('Kirjaudu sisään'), findsOneWidget);
 
       // Try logging in with test credentials.
-      final Finder debugTeacherButton = find.byKey(const ValueKey("debug teacher"));
-      await tester.tap(debugTeacherButton);
+      // final Finder debugUserButton = find.byKey(const ValueKey("debug teacher"));
+      final Finder debugUserButton = find.byKey(const ValueKey("debug student"));
+      await tester.tap(debugUserButton);
       await tester.pumpAndSettle(const Duration(seconds: 1));
       /* USE THESE LINES FOR LOG IN ONCE DEBUG USERS ARE REMOVED FROM THE LOG IN -PAGE:
       await tester.enterText(find.byKey(const ValueKey("e-mail")), 'olli.o@esimerkki.fi');
@@ -130,6 +130,81 @@ void main() {
       await tester.tap(logOutButton);
       await tester.pumpAndSettle(const Duration(seconds: 1));
       expect(find.text('Kirjaudu sisään'), findsWidgets);
+    });
+    testWidgets('New report page', (tester) async {app.main();
+      await tester.pumpAndSettle(const Duration(seconds: 5));
+      expect(find.text('Kirjaudu sisään'), findsOneWidget);
+
+      // Try logging in with test credentials.
+      final Finder debugStudentButton = find.byKey(const ValueKey("debug student"));
+      await tester.tap(debugStudentButton);
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+      /* USE THESE LINES FOR LOG IN ONCE DEBUG USERS ARE REMOVED FROM THE LOG IN -PAGE:
+      await tester.enterText(find.byKey(const ValueKey("e-mail")), 'olli.o@esimerkki.fi');
+      await tester.enterText(find.byKey(const ValueKey("password")), 'salasana');
+      */
+      final Finder loginButton2 = find.text('Kirjaudu');
+      await tester.tap(loginButton2);
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+
+      // Go to reports -page via sidebar
+      final Finder drawer = find.byTooltip('Open navigation menu');
+      expect(drawer, findsWidgets);
+      await tester.tap(drawer);
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+      final Finder goToIcon = find.byIcon(Icons.speaker_notes_outlined);
+      await tester.tap(goToIcon);
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+      // Go to new report -page via floating button
+      for (var i=0; i<2; i++) {
+        expect(find.text('Ilmoitukset'), findsWidgets);
+        expect(find.byType(FloatingActionButton), findsOneWidget);
+        await tester.tap(find.byType(FloatingActionButton));
+        await tester.pumpAndSettle(const Duration(seconds: 1));
+        if (i==0) {
+          expect(find.byTooltip('Back'), findsOneWidget);
+          await tester.tap(find.byTooltip('Back'));
+          await tester.pumpAndSettle(const Duration(seconds: 1));
+        }
+        else {
+          await tester.enterText(find.byKey(const ValueKey("Description")), 
+          "Ville löi Elliä ruokatunnilla.");
+          await tester.enterText(find.byKey(const ValueKey("Bully")), 
+          'Ville Vitosluokkalainen');
+
+          final bulliedCheckboxFinder = find.byKey(const ValueKey("BulliedCheckbox"));
+          var bulliedCheckbox = tester.firstWidget(bulliedCheckboxFinder) as CheckboxListTile;
+          expect(bulliedCheckbox.value, false);
+          // TODO: make check that the text input is not possible when Checkbox is unchecked.
+          await tester.tap(bulliedCheckboxFinder);
+          await tester.pumpAndSettle();
+          /*await tester.enterText(find.byKey(const ValueKey("Bullied")),
+          'Elli Eskarilainen');
+          await tester.pumpAndSettle();*/
+          //expect(find.text("Elli Eskarilainen"), findsOneWidget);
+          final teacherDrobdownFinder = find.byKey(const ValueKey("SendTo"));
+          await tester.tap(teacherDrobdownFinder);
+          await tester.pumpAndSettle();
+          final dropdownItem = find.text('Olli Opettaja').last;
+          await tester.tap(dropdownItem);
+          await tester.pumpAndSettle();
+          final anonCheckboxFinder = find.byKey(const ValueKey("AnonCheckbox"));
+          var anonCheckbox = tester.firstWidget(anonCheckboxFinder) as CheckboxListTile;
+          expect(anonCheckbox.value, false);
+          await tester.tap(anonCheckboxFinder);
+          await tester.pumpAndSettle();
+          // expect(anonCheckbox.value, true); does not work as expected, why?
+          final sendReportButton = find.text('Lähetä');
+          await tester.tap(sendReportButton);
+          await tester.pumpAndSettle();
+          expect(find.text("Ilmoitukset"), findsOneWidget);
+          final sentReportFinder = find.textContaining("Ville löi");
+          await tester.tap(sentReportFinder);
+          await tester.pumpAndSettle();
+          expect(find.textContaining("Ville löi"), findsOneWidget);
+          await tester.pumpAndSettle(const Duration(seconds: 10));
+
+        }}
     });
   });
 }
